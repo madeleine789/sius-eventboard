73eca71e65f43f4f6690c2441cf00870b7a4afd5
madeleine789 <madeleine789@gmail.com>
2016-05-02 12:06:50 +0200

    Added Twitter oauth authentication.

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ __author__ = 'mms'
 
 from flask.ext.cache import Cache
 from flask.ext.twitter_oembedder import TwitterOEmbedder
-
-from flask import Flask, g
+from flask_oauth import OAuth
+from flask import Flask
 from flask.ext.mongoengine import MongoEngine, MongoEngineSessionInterface
 from flask.ext.login import LoginManager
 from flask.ext.bcrypt import Bcrypt
@@ -13,9 +13,9 @@ from pymongo import ReadPreference
 app = Flask("FlaskLoginApp")
 
 app.config['MONGODB_SETTINGS'] = {
-	'read_preference': ReadPreference.PRIMARY,
-	'db': 'eventboard',
-	#'host':'mongodb://user:eventboard@ds029317.mongolab.com:29317/eventboard'
+    'read_preference': ReadPreference.PRIMARY,
+    'db': 'eventboard',
+    # 'host':'mongodb://user:eventboard@ds029317.mongolab.com:29317/eventboard'
 }
 
 app.config['THREADS_PER_PAGE'] = 4
@@ -26,7 +26,6 @@ app.config['TWITTER_CONSUMER_KEY'] = 'IRh7xn4AZfryNrsXQI3hXNIPu'
 app.config['TWITTER_CONSUMER_SECRET'] = 'YhJ0JWYoS7am327ST6PQVkdxmbkvI10A5dIzJs3xv5FWNrgDjV'
 app.config['TWITTER_ACCESS_TOKEN'] = '3305385699-0WEBODjVEuMn48QaK80hPCkA7xTJGNk2CU21YSc'
 app.config['TWITTER_TOKEN_SECRET'] = 'g8cgsMCNtx8cohb3tNccJX2gEaKZ9u8QcqY1a8wKV7SiK'
-
 
 db = MongoEngine(app)
 app.session_interface = MongoEngineSessionInterface(db)
@@ -41,7 +40,14 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.refresh_view = 'auth_app.login'
 
+oauth = OAuth()
+twitter = oauth.remote_app('twitter',
+                           base_url='https://api.twitter.com/1/',
+                           request_token_url='https://api.twitter.com/oauth/request_token',
+                           access_token_url='https://api.twitter.com/oauth/access_token',
+                           authorize_url='https://api.twitter.com/oauth/authenticate',
+                           consumer_key=app.config['TWITTER_CONSUMER_KEY'],
+                           consumer_secret=app.config['TWITTER_CONSUMER_SECRET']
+                           )
+
 app.debug = True
-
-
-
